f11deb277bdda28ab0fe6f8282f10dd5910f2dd6
M. Sertaç Toröz <54281068+sertactoroz@users.noreply.github.com>
2022-12-07 16:50:45 +0300

    blog is moved from home to blogpage

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,17 @@
 import Head from "next/head";
 import Link from "next/link";
+import utilStyles from '../../styles/utils.module.css';
+import { getSortedPostsData } from '../../lib/posts'
 
-export default function Posts() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Posts({ allPostsData }) {
   return (
     <div>
       <Head>
@@ -9,6 +19,21 @@ export default function Posts() {
       </Head>
       <h3>Blog Page</h3>
       <Link href="/">Back to home</Link>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2> 
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
+    
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
 
-export default function Home({ allPostsData }) {
+
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -20,23 +12,9 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p> Hello I am Sertaç. I'm a developer and a carpenter</p>
-      
       </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2> 
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
+      
       
     </Layout>
   );
